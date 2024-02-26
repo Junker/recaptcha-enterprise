e4b7febfd9e94b0c3c0bf867dbb13d7d0a4f1fe6
Junker <1144095+Junker@users.noreply.github.com>
2024-02-26 21:01:26 +0700

    fixes

--- recaptcha-enterprise.lisp
+++ recaptcha-enterprise.lisp
@@ -10,29 +10,29 @@
   (assert (not (null *project-id*)))
   (let* ((response (dex:post (format nil "https://recaptchaenterprise.googleapis.com/v1/projects/~A/assessments?key=~A"
                                      *project-id* *api-key*)
-                             :headers `(("Content-Type" . "application/json"))
+                             :headers '(("Content-Type" . "application/json"))
                              :content (jojo:to-json (list :|event| (list :|token| token
                                                                          :|siteKey| key-id
                                                                          :|expectedAction| action))))))
     (jojo:parse response)))
 
 (defun risk-analysis (response)
-  (getf :|riskAnalysis| response))
+  (getf response :|riskAnalysis|))
 
 (defun token-properties (response)
-  (getf :|tokenProperties| response))
+  (getf response :|tokenProperties|))
 
 (defun event (response)
-  (getf :|event| response))
+  (getf response :|event|))
 
 (defun name (response)
-  (getf :|name| response))
+  (getf response :|name| ))
 
 (defun validp (response)
-  (getf :|valid| (token-properties response)))
+  (getf (token-properties response) :|valid|))
 
 (defun invalid-reason (response)
-  (getf :|invalidReason| (token-properties response)))
+  (getf (token-properties response) :|invalidReason|))
 
 (defun score (response)
-  (getf :|score| (risk-analysis response)))
+  (getf (risk-analysis response) :|score|))
